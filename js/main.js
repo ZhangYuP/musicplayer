454a839fc665e63be0581a25d06f245e4b0ad59a
ZhangYuP <zyp5318@qq.com>
2019-06-19 23:29:10 +0800

    实现播放功能

--- js/main.js
+++ js/main.js
@@ -7,11 +7,6 @@ var EventCenter = {
   }
 }
 
-//EventCenter.on('hello', function(e){
-//  console.log(e.detail)
-//})
-
-//EventCenter.fire('hello', '你好')
 
 var Footer = {
   init() {
@@ -20,6 +15,7 @@ var Footer = {
     this.$box = this.$footer.find('.box')
     this.$leftBtn = this.$footer.find('.icon-left')
     this.$rightBtn = this.$footer.find('.icon-right')
+    this.isAnimate = false
     this.bind()
     this.render()
   },
@@ -30,18 +26,26 @@ var Footer = {
     })
 
     this.$rightBtn.on('click', ()=>{
+      if(this.isAnimate) return
       var width = this.$footer.find('li').outerWidth(true)
       var distance = parseInt(this.$box.width() / width) * width
       if(this.$ul.position().left + this.$ul.width() > distance){
-        this.$ul.animate({left: '-=' + distance}, 400)
+        this.isAnimate = true
+        this.$ul.animate({left: '-=' + distance}, 400, ()=>{
+          this.isAnimate = false
+        })
       }      
     })
 
     this.$leftBtn.on('click', ()=>{
+      if(this.isAnimate) return
       var width = this.$footer.find('li').outerWidth(true)
       var distance = parseInt(this.$box.width() / width) * width
       if(this.$ul.position().left < 0){
-        this.$ul.animate({left: '+=' + distance}, 400)
+        this.isAnimate = true
+        this.$ul.animate({left: '+=' + distance}, 400, ()=>{
+          this.isAnimate = false
+        })
       }
     })
     
@@ -49,17 +53,25 @@ var Footer = {
       $(this).addClass('active')
         .siblings().removeClass('active')
       
-      EventCenter.fire('select-album', $(this).attr('data-channel-id'))
+      EventCenter.fire('select-album', {
+        channelId: $(this).attr('data-channel-id'),
+        channelName: $(this).attr('data-channel-name')
+      })
+    })
+
+    EventCenter.on('reset-album', (e, channelId)=>{
+      this.$footer.find(`[data-channel-id=${channelId}]`).addClass('active')
+      .siblings().removeClass('active')
     })
   },
   
   render() {
     $.ajax({
-      url: 'http://api.jirengu.com/fm/getChannels.php',
-      method: 'get'
+      url: 'https://jirenguapi.applinzi.com/fm/getChannels.php',
+      dataType: 'json'
     }).then(
       (ret)=>{
-        this.renderFooter(JSON.parse(ret).channels)
+        this.renderFooter(ret.channels)
       },
       ()=>{console.log('error')
     })
@@ -68,7 +80,7 @@ var Footer = {
   renderFooter(channels) {
     var html = ''
     channels.forEach(function(channel){
-      html += `<li data-channel-id="${channel.channel_id}"><div class="cover" style="background-image:url('${channel.cover_small}')"></div><h3>${channel.name}</h3></li>`
+      html += `<li data-channel-id="${channel.channel_id}" data-channel-name="${channel.name}"><div class="cover" style="background-image:url('${channel.cover_small}')"></div><h3>${channel.name}</h3></li>`
     })
     this.$ul.html(html)
     this.setStyle()
@@ -84,16 +96,88 @@ var Footer = {
 
 }
 
-var App = {
+var Fm = {
   init() {
+    this.$main = $('main')
+    this.audio = new Audio()
+    this.audio.autoplay = 'autoplay'
     this.bind()
+    this.list = []
   },
   bind() {
-    EventCenter.on('select-album', function(e, data){
-      console.log('select ', data)
+    var _this = this
+    EventCenter.on('select-album', function(e, options){
+      _this.channelId = options.channelId
+      _this.channelName = options.channelName
+      _this.loadMusic()
     })
+    this.$main.find('.btn-play').on('click', function(){
+      var $btn = $(this)
+      if($btn.hasClass('icon-play')){
+        $btn.removeClass('icon-play').addClass('icon-pause')
+        _this.audio.play()
+      }else{
+        $btn.removeClass('icon-pause').addClass('icon-play')
+        _this.audio.pause()
+      }
+    })
+    this.$main.find('.btn-next').on('click', function(){
+      _this.loadMusic()
+    })
+    this.$main.find('.btn-pre').on('click', function(){
+      if(_this.list.length > 1){
+        _this.list.pop()
+        _this.song = _this.list[_this.list.length - 1].song
+        _this.channelId = _this.list[_this.list.length - 1].channelId
+        _this.channelName = _this.list[_this.list.length - 1].channelName
+        _this.setMusic()
+
+        EventCenter.fire('reset-album', _this.channelId)
+      }
+    })
+    this.audio.addEventListener('play', function(){
+      clearInterval(_this.statusClock)
+      _this.statusClock = setInterval(function(){
+        _this.updateStatus()
+      }, 1000)
+    })
+    this.audio.addEventListener('pause', function(){
+      clearInterval(_this.statusClock)
+    })
+
+  },
+  loadMusic() {
+    $.ajax({
+      url: 'https://jirenguapi.applinzi.com/fm/getSong.php',
+      dataType: 'json',
+      data: {channel: 'this.channelId'}
+    }).then((ret)=>{
+      this.song = ret['song'][0]
+      if(!this.song.url){
+        this.loadMusic()
+      }else{
+        this.setMusic()
+        this.list.push({song: this.song, channelId: this.channelId, channelName: this.channelName})
+      }
+    })
+  },
+  setMusic() {
+    this.audio.src = this.song.url
+    $('.bg').css('background-image', `url(${this.song.picture})`)
+    this.$main.find('aside figure').css('background-image', `url(${this.song.picture})`)
+    this.$main.find('.info .tag').text(this.channelName)
+    this.$main.find('.info h1').text(this.song.title)
+    this.$main.find('.info .artist').text(this.song.artist)
+    this.$main.find('.info .lrc').text(this.song.lrc)
+  },
+  updateStatus() {
+    var min = Math.floor(this.audio.currentTime / 60)
+    var second = Math.floor(this.audio.currentTime % 60) + ''
+    second = second.length === 2 ? second : '0' + second
+    this.$main.find('.currentTime').text(min + ':' + second)
+    this.$main.find('.progress').width(this.audio.currentTime/this.audio.duration*100 + '%')
   }
 }
 
 Footer.init()
-App.init()
+Fm.init()
